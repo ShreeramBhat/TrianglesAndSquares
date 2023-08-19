38b910013de15057cda6b6696e1ee6dec5a85080
Shreeram Bhat <shreeram@winds.co.in>
2023-08-20 00:10:50 +0530

    Code cleanup

--- TrianglesAndSquares/ViewController.swift
+++ TrianglesAndSquares/ViewController.swift
@@ -15,72 +15,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-//        let width = self.view.frame.width
-//        self.addEquilateralTriangle(startPoint: CGPoint(x: 0, y: width), width: width)
-//        let triangleHeight: CGFloat = (sqrt(3) / 2) * width
-//        let diff = width - triangleHeight
-//        self.addSquare(startPoint: CGPoint(x: width / 4, y: (triangleHeight/2) + diff), width: width/2)
-        
-//        let nestedShapesView = RecursiveNestedShapesView()
-//        nestedShapesView.backgroundColor = .white
-//        nestedShapesView.translatesAutoresizingMaskIntoConstraints = false
-//        view.addSubview(nestedShapesView)
-//
-//        NSLayoutConstraint.activate([
-//            nestedShapesView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-//            nestedShapesView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-//            nestedShapesView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 1),
-//            nestedShapesView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 1)
-//        ])
-        
-//        let recursiveNestedShapesView = RecursiveNestedShapesView()
-//        recursiveNestedShapesView.backgroundColor = .white
-//        recursiveNestedShapesView.translatesAutoresizingMaskIntoConstraints = false
-//        view.addSubview(recursiveNestedShapesView)
-//
-//        NSLayoutConstraint.activate([
-//            recursiveNestedShapesView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-//            recursiveNestedShapesView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-//            recursiveNestedShapesView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.8),
-//            recursiveNestedShapesView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.8)
-//        ])
-        
-//        let equilateralTriangleView = EquilateralTriangleView()
-//        equilateralTriangleView.backgroundColor = .white
-//        equilateralTriangleView.translatesAutoresizingMaskIntoConstraints = false
-//        view.addSubview(equilateralTriangleView)
-//        
-//        NSLayoutConstraint.activate([
-//            equilateralTriangleView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-//            equilateralTriangleView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-//            equilateralTriangleView.widthAnchor.constraint(equalToConstant: 200),
-//            equilateralTriangleView.heightAnchor.constraint(equalTo: equilateralTriangleView.widthAnchor, multiplier: sqrt(3) / 2)
-//        ])
-//
-        
-
-//        let squareView = SquareView()
-//        squareView.backgroundColor = .white
-////        squareView.translatesAutoresizingMaskIntoConstraints = false
-//        view.addSubview(squareView)
-//
-//        let length: CGFloat = (sqrt(3) * width) / (2 + sqrt(3))
-//        let squareOrigin = CGPoint(x: self.view.frame.midX - length / 2, y: self.view.frame.maxY - length)
-//
-//        squareView.frame.origin = squareOrigin
-        
-//        NSLayoutConstraint.activate([
-////            squareView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-////            squareView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-////            squareView.leadingAnchor.constraint(equalTo: triangleWithSquareView.leadingAnchor, constant: squareOrigin.x),
-//
-//            squareView.widthAnchor.constraint(equalToConstant: length),
-//            squareView.heightAnchor.constraint(equalTo: squareView.widthAnchor)
-//        ])
-        
         let width: CGFloat = 200
-//        self.addTriangleWithSquareView(width: width)
-        
         let contentView = UIView(frame: CGRect(origin: .zero, size: CGSize(width: width, height: width)))
         contentView.backgroundColor = .clear
         contentView.translatesAutoresizingMaskIntoConstraints = false
@@ -113,7 +48,6 @@ class ViewController: UIViewController {
 
         NSLayoutConstraint.activate([
             triangleWithSquareView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-//            triangleWithSquareView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
             triangleWithSquareView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
             triangleWithSquareView.widthAnchor.constraint(equalToConstant: width),
             triangleWithSquareView.heightAnchor.constraint(equalTo: triangleWithSquareView.widthAnchor, multiplier: sqrt(3) / 2)
@@ -122,37 +56,5 @@ class ViewController: UIViewController {
         return triangleWithSquareView
     }
     
-//    private func addEquilateralTriangle(startPoint: CGPoint, width: CGFloat) {
-//        let path = CGMutablePath()
-//        path.move(to: startPoint)
-//        let triangleHeight: CGFloat = (sqrt(3) / 2) * width
-//        path.addLine(to: CGPoint(x: width/2, y: startPoint.y - triangleHeight))
-//        path.addLine(to: CGPoint(x: width, y: startPoint.y))
-//        path.addLine(to: startPoint)
-//
-//        self.addShape(for: path)
-//    }
-//
-//    private func addSquare(startPoint: CGPoint, width: CGFloat) {
-//        let path = CGMutablePath()
-//        path.move(to: startPoint)
-//        path.addLine(to: CGPoint(x: startPoint.x + width, y: startPoint.y))
-//        path.addLine(to: CGPoint(x: startPoint.x + width, y: startPoint.y + width))
-//        path.addLine(to: CGPoint(x: startPoint.x, y: startPoint.y + width))
-//        path.addLine(to: startPoint)
-//
-//        self.addShape(for: path)
-//    }
-//
-//    private func addShape(for path: CGPath) {
-//        let shape = CAShapeLayer()
-//        shape.path = path
-//        shape.lineWidth = 1
-//        shape.strokeColor = UIColor.black.cgColor
-//        shape.fillColor = UIColor.white.cgColor
-//
-//        self.view.layer.addSublayer(shape)
-//    }
-
 }
 
